34d3c9d2ff973bf9e554d173408aab3038bf6d52
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-09-06 14:29:04 -0300

    feat: Added the initial setup
    Created the project, added the assets for the project, added the login with clerk, added the fonts and colors (CSS)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # team4-desafio2-plant-peace
+
+## Para acessar a documentação do projeto acesse a [Wiki](https://github.com/talessoares/team4-desafio2-plant-peace/wiki)
+
+## Como Executar: 
+
+...
---
+++ plant-peace-web/clerk-react/src/App.tsx
@@ -0,0 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
+
+export default App;
