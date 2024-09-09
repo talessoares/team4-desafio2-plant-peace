b4e20c9e0a0b081502520f039d18cd7dabaad1fc
Mendes113 <andremendes0113@gmail.com>
2024-09-09 13:41:08 -0300

    fix: update plant model

--- plant-peace-api/src/models/plant.ts
+++ plant-peace-api/src/models/plant.ts
@@ -1,13 +1,27 @@
-export interface Plant {
-    id: number;
-    name: string;
-    subtitle: string;
-    label: string[];
-    price: string;
-    isInSale: boolean;
-    discountPercentage?: number;
-    features: string;
-    description: string;
-    imgUrl: string;
-  }
-  
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IPlant extends Document {
+  name: string;
+  subtitle: string;
+  label: string[];
+  price: string;
+  isInSale: boolean;
+  discountPercentage?: number;
+  features: string;
+  description: string;
+  imgUrl: string;
+}
+
+const plantSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  subtitle: { type: String, required: true },
+  label: { type: [String], required: true },
+  price: { type: String, required: true },
+  isInSale: { type: Boolean, required: true },
+  discountPercentage: { type: Number },
+  features: { type: String, required: true },
+  description: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+});
+
+export default mongoose.model<IPlant>('Plant', plantSchema);
