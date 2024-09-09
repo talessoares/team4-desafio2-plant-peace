aed2447e5525e843c6a8262d87ec8423ea7c0bce
Mendes113 <andremendes0113@gmail.com>
2024-09-09 13:42:20 -0300

    fix: Update service to use types

--- plant-peace-api/src/services/plantService.ts
+++ plant-peace-api/src/services/plantService.ts
@@ -1,27 +1,29 @@
-import { Plant } from '../models/plant';
+import { IPlant } from '../models/plant';
 
-const plants: Plant[] = [];
+const plants: IPlant[] = [];
 
-export const getPlants = (): Plant[] => {
+export const getPlants = (): IPlant[] => {
   return plants;
 };
 
-export const createPlant = (plant: Plant): void => {
+export const createPlant = (plant: IPlant): void => {
   plants.push(plant);
 };
 
-export const getPlantById = (id: number): Plant | undefined => {
+export const getPlantById = (id: number): IPlant | undefined => {
   return plants.find((plant) => plant.id === id);
 };
 
-export const updatePlant = (updatedPlant: Plant): void => {
+export const updatePlant = (updatedPlant: IPlant): void => {
   const index = plants.findIndex((plant) => plant.id === updatedPlant.id);
-  plants[index] = updatedPlant;
+  if (index !== -1) {
+    plants[index] = updatedPlant;
+  }
 };
 
 export const deletePlant = (id: number): void => {
   const index = plants.findIndex((plant) => plant.id === id);
-  plants.splice(index, 1);
+  if (index !== -1) {
+    plants.splice(index, 1);
+  }
 };
-
-
