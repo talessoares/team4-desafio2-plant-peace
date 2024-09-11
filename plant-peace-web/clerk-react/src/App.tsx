7fa3a0c35379011fc7e2cb3fbc0c4cb4d76944c9
Eduardo Dorneles <ce.carloseduardo3993@gmail.com>
2024-09-11 15:13:05 -0300

    feat: added the component SectionSteps

--- plant-peace-web/clerk-react/src/App.tsx
+++ plant-peace-web/clerk-react/src/App.tsx
@@ -1,22 +1,55 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-
+// import {
+//   SignedIn,
+//   SignedOut,
+//   SignInButton,
+//   UserButton,
+// } from "@clerk/clerk-react";
 import "./App.css";
+import SectionSteps from "./components/SectionSteps/SectionSteps";
+import water from "./assets/icons/water.png";
+import sun from "./assets/icons/sun.png";
+import nutrients from "./assets/icons/nutrients.png";
 
 function App() {
+  const cards = [
+    {
+      icon: water,
+      title: "Watering",
+      description: `water your plants when the top inch of soil feels dry to the touch.
+        Avoid overwatering, as it can lead to root dehydration.`,
+    },
+    {
+      icon: sun,
+      title: "Sunlight",
+      description: `Most plants need adequate sunlight to thrive. Place your plants in
+            areas that receive the appropriate amount of light for their
+            specific needs`,
+    },
+    {
+      icon: nutrients,
+      title: "Nutrients and Fertilizing",
+      description: `Choose a suitable fertilizer based on the specific needs of your
+            plants, whether it's a balanced or specialized formula.`,
+    },
+  ];
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <>
+      {/* <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header> */}
+      <SectionSteps
+        title="Steps To Take Care Of Your Plants"
+        description="By following these three steps - proper watering, appropriate
+          sunlight, and providing essential nutrients - you'll be well on your
+          way to maintaining healthy and thriving plants."
+        cards={cards}
+      />
+    </>
   );
 }
 
