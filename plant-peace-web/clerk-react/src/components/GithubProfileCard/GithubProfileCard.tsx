8d968ac3ebf08d1ae0e51fb78720b70df76bde18
Mendes113 <andremendes0113@gmail.com>
2024-09-14 12:54:31 -0300

    fix: Fix import name causing error

--- plant-peace-web/clerk-react/src/components/GithubProfileCard/GithubProfileCard.tsx
+++ plant-peace-web/clerk-react/src/components/GithubProfileCard/GithubProfileCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./GitHubProfileCard.module.css";
+import styles from "./GithubProfileCard.module.css";
 
 interface GitHubProfileCardProps {
   username: string;
