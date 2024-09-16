580b0834a60479824842f0d1dc2b6fd9aa1a16df
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-09-16 15:14:01 -0300

    fix: standardize quotes and improve code formatting in Sale, ErrorPage, and Home components

--- plant-peace-web/clerk-react/src/pages/Error/ErrorPage.tsx
+++ plant-peace-web/clerk-react/src/pages/Error/ErrorPage.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './ErrorPage.module.css';
+import { Link } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   return (
@@ -8,20 +7,22 @@ const ErrorPage = () => {
       <div className={styles.errorContent}>
         <h1 className={styles.errorCode}>404</h1>
         <p className={styles.errorMessage}>Oops! Page not found.</p>
-        
+
         <div className={styles.gifContainer}>
           <iframe
             src="https://giphy.com/embed/XSTtrAN0rJfy"
             width="100%"
             height="100%"
-            style={{ position: 'absolute' }}
+            style={{ position: "absolute" }}
             frameBorder="0"
             className={styles.giphyEmbed}
             allowFullScreen
           ></iframe>
         </div>
-        
-        <Link to="/" className={styles.goHome}>Go to Homepage</Link>
+
+        <Link to="/" className={styles.goHome}>
+          Go to Homepage
+        </Link>
       </div>
     </div>
   );
