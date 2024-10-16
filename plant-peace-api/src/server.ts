f18d036dd49fd7f942bda4d10535499d05eae5b3
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-10-16 15:52:01 -0400

    fix: fix cors

--- plant-peace-api/src/server.ts
+++ plant-peace-api/src/server.ts
@@ -1,14 +1,6 @@
 import app from "./app";
-import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-  origin: "http://plant-peace-app.s3-website.us-east-2.amazonaws.com",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
