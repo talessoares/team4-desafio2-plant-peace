72da0e5e9c1362640ac0510250be27cadf9c9710
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-10-18 19:39:15 -0400

    deleted: deleted multer and path import on app.ts

--- plant-peace-api/src/app.ts
+++ plant-peace-api/src/app.ts
@@ -3,8 +3,6 @@ import plantRoutes from "./routes/plantRoutes";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
 import cors from "cors";
-import multer from "multer";
-import path from "path";
 
 dotenv.config();
 
