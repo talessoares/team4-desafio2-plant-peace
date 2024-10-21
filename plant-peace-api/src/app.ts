291c3cd2f0a92f2d238baca9642383552e5556d9
wuesley <wuesleynow7@gmail.com>
2024-10-21 14:56:02 -0300

    fix: adjusted .gitignore and added configuration

--- plant-peace-api/src/app.ts
+++ plant-peace-api/src/app.ts
@@ -3,8 +3,7 @@ import plantRoutes from "./routes/plantRoutes";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
 import cors from "cors";
-import multer from "multer";
-import path from "path";
+
 
 dotenv.config();
 
@@ -13,6 +12,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
